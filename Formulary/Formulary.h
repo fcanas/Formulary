c72f378184b783244f51916620300247a2b47513
Fabian Canas <fcanas@gmail.com>
2015-01-16 10:59:24 -0500

    Initial Commit

---
+++ Formulary/Formulary.h
@@ -0,0 +1,19 @@
+//
+//  Formulary.h
+//  Formulary
+//
+//  Created by Fabian Canas on 1/16/15.
+//  Copyright (c) 2015 Fabian Canas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Formulary.
+FOUNDATION_EXPORT double FormularyVersionNumber;
+
+//! Project version string for Formulary.
+FOUNDATION_EXPORT const unsigned char FormularyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Formulary/PublicHeader.h>
+
+
