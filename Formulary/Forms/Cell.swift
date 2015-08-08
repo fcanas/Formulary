657b7bd648d31a2b8b96dd9d9af26196b59977cb
Timm Preetz <timm@lunados.com>
2015-08-08 18:58:01 +0200

    Use specific DecimalTableViewCell for decimal cell that does not allow non-decimal input characters

--- Formulary/Forms/Cell.swift
+++ Formulary/Forms/Cell.swift
@@ -32,69 +32,15 @@ extension UITableView {
         
         self.registerClass(FormTableViewCell.self, forCellReuseIdentifier: FormRowType.Text.rawValue)
         self.registerClass(FormTableViewCell.self, forCellReuseIdentifier: FormRowType.Number.rawValue)
-        self.registerClass(FormTableViewCell.self, forCellReuseIdentifier: FormRowType.Decimal.rawValue)
+        self.registerClass(DecimalTableViewCell.self, forCellReuseIdentifier: FormRowType.Decimal.rawValue)
         self.registerClass(FormTableViewCell.self, forCellReuseIdentifier: FormRowType.Email.rawValue)
         self.registerClass(FormTableViewCell.self, forCellReuseIdentifier: FormRowType.Twitter.rawValue)
         self.registerClass(FormTableViewCell.self, forCellReuseIdentifier: FormRowType.URL.rawValue)
     }
 }
 
-func configureCell(cell: FormTableViewCell, inout row: FormRow) {
-    
-    cell.action = nil
-    cell.formRow = row
-    
-    switch row.type {
-    case .Plain:
-        cell.textLabel?.text = row.name
-        break
-    case .Switch:
-        cell.textLabel?.text = row.name
-        let s = UISwitch()
-        cell.accessoryView = s
-        ActionTarget(control: s, action: { _ in
-            row.value = s.on
-        })
-        
-        if let enabled = row.value as? Bool {
-            s.on = enabled
-        }
-    case .Toggle:
-        cell.textLabel?.text = row.name
-        cell.accessoryType = ((row.value as? Bool) ?? false) ? UITableViewCellAccessoryType.Checkmark : .None
-        
-        cell.action = { x in
-            cell.accessoryType = ((row.value as? Bool) ?? false) ? UITableViewCellAccessoryType.Checkmark : .None
-        }
-        
-    case .Button:
-        let button = UIButton(frame: cell.bounds)
-        
-        button.setTitle(row.name, forState: .Normal)
-        button.setTitleColor(cell.tintColor, forState: .Normal)
-
-        button.setTranslatesAutoresizingMaskIntoConstraints(false)
-        cell.contentView.addSubview(button)
-        cell.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[button]-|", options: nil, metrics: nil, views: ["button":button]))
-        cell.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[button]-|", options: nil, metrics: nil, views: ["button":button]))
-        
-        var emptyAction :Action = { _ in }
-        
-        ActionTarget(control: button, controlEvents: UIControlEvents.TouchUpInside, action: row.action ?? emptyAction)
-    case .Text:
-        configureTextCell(cell, &row).keyboardType = .Default
-    case .Number:
-        configureTextCell(cell, &row).keyboardType = .NumberPad
-    case .Decimal:
-        configureTextCell(cell, &row).keyboardType = .DecimalPad
-    case .Email:
-        configureTextCell(cell, &row).keyboardType = .EmailAddress
-    case .Twitter:
-        configureTextCell(cell, &row).keyboardType = .Twitter
-    case .URL:
-        configureTextCell(cell, &row).keyboardType = .URL
-    }
-    cell.selectionStyle = .None
+protocol FormTableViewCellType {
+    func configureCell(inout row: FormRow)
 }
 
 func configureTextCell(cell: FormTableViewCell, inout row: FormRow) -> UITextField {
@@ -124,11 +70,78 @@ func configureTextCell(cell: FormTableViewCell, inout row: FormRow) -> UITextFie
     return textField!
 }
 
-class FormTableViewCell: UITableViewCell {
+class FormTableViewCell: UITableViewCell, FormTableViewCellType {
     var configured: Bool = false
     var formRow: FormRow?
     var action :Action?
     var textField :NamedTextField?
+    
+    func configureCell(inout row: FormRow) {
+        self.action = nil
+        self.formRow = row
+        
+        switch row.type {
+        case .Plain:
+            self.textLabel?.text = row.name
+            break
+        case .Switch:
+            self.textLabel?.text = row.name
+            let s = UISwitch()
+            self.accessoryView = s
+            ActionTarget(control: s, action: { _ in
+                row.value = s.on
+            })
+            
+            if let enabled = row.value as? Bool {
+                s.on = enabled
+            }
+        case .Toggle:
+            self.textLabel?.text = row.name
+            self.accessoryType = ((row.value as? Bool) ?? false) ? UITableViewCellAccessoryType.Checkmark : .None
+            
+            self.action = { x in
+                self.accessoryType = ((row.value as? Bool) ?? false) ? UITableViewCellAccessoryType.Checkmark : .None
+            }
+            
+        case .Button:
+            let button = UIButton(frame: self.bounds)
+            
+            button.setTitle(row.name, forState: .Normal)
+            button.setTitleColor(self.tintColor, forState: .Normal)
+            
+            button.setTranslatesAutoresizingMaskIntoConstraints(false)
+            self.contentView.addSubview(button)
+            self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[button]-|", options: nil, metrics: nil, views: ["button":button]))
+            self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[button]-|", options: nil, metrics: nil, views: ["button":button]))
+            
+            var emptyAction :Action = { _ in }
+            
+            ActionTarget(control: button, controlEvents: UIControlEvents.TouchUpInside, action: row.action ?? emptyAction)
+        case .Text:
+            configureTextCell(self, &row).keyboardType = .Default
+        case .Number:
+            configureTextCell(self, &row).keyboardType = .NumberPad
+        case .Decimal:
+            configureTextCell(self, &row).keyboardType = .DecimalPad
+        case .Email:
+            configureTextCell(self, &row).keyboardType = .EmailAddress
+        case .Twitter:
+            configureTextCell(self, &row).keyboardType = .Twitter
+        case .URL:
+            configureTextCell(self, &row).keyboardType = .URL
+        }
+        self.selectionStyle = .None
+    }
+}
+
+class DecimalTableViewCell: FormTableViewCell {
+    let inputDelegate = DecimalInputTextFieldDelegate()
+
+    override func configureCell(inout row: FormRow) {
+        super.configureCell(&row)
+        
+        self.textField?.delegate = inputDelegate
+    }
 }
 
 let ActionTargetControlKey :UnsafePointer<Void> = UnsafePointer<Void>()
